5c39d137eddf9f5c09b784589a96283b6df0e8a3
MrSunRise1 <mrsunrisecom@gmail.com>
2024-10-14 21:14:48 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,22 @@
+print("Задача:Выписывать из этого списка только положительные числа до тех пор,"
+      " пока не встретите отрицательное или не закончится список"
+      )
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+print(my_list)
+confirmation = input("Вы готовы начать?Напишите 'Да' или 'Нет' : ")
+if confirmation == 'Да' or confirmation == 'да' or confirmation == 'ДА' :
+    print (my_list)
+    print("Ответ: ")
+    first = 0
+    while first < len (my_list) :
+        if my_list[first] >= 0 :
+            if my_list[first] != 0 :
+                print (my_list[first])
+                first = first + 1
+            elif my_list[first] == 0 :
+                first = first + 1
+                continue
+            else:
+                break
+else:
+    print ("До свидания!")
