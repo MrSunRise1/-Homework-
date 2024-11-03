ca8bd6dc8f352e27a1c654d418f6634c14f18392
MrSunRise1 <mrsunrisecom@gmail.com>
2024-11-03 13:12:17 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+from fake_math import divide as fakkkke
+from true_math import divide as truuuue
+
+
+result1 = fakkkke(2,0)
+result2 = fakkkke(54,9)
+result3 = truuuue(48,12)
+result4 = truuuue(99,0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
